a6ff88a007e0ef1a57bbc030ff2d50fabaaa7d73
ThiagoShowza <61065518+ThiagoShowza@users.noreply.github.com>
2022-06-17 11:15:47 -0300

    Add files via upload
    Commit arquivo Laboratorio

--- Laboratorio.java
+++ Laboratorio.java
@@ -1,21 +1,49 @@
 package provaArvoreBinaria;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Laboratorio {
 
 	public static void main(String[] args) {
-		ArvoreBinaria arvore = new ArvoreBinaria();
+		
+		Scanner leitor = new Scanner(System.in);
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        Random gerador = new Random();
+       
+        int grau, escolha;
+        
+        //CADA NÚMERO REPRESENTA UM GRAU DE PARENTESCO, E O USUÁRIO TERÁ A OPÇÃO DE EXCLUIR ESSE GRAU DA FORMA QUE DESEJAR;
+        //O PROGRAMA AUTOMATICAMENTE VAI PUXAR OS DADOS DE CONSULTA, E IRÁ INFORMA-LOS NA TELA;
+        
+		arvore.insereValor(arvore.raiz, gerador.nextInt(100)+1);
+		arvore.insereValor(arvore.raiz, gerador.nextInt(100)+1);
+		arvore.insereValor(arvore.raiz, gerador.nextInt(100)+1);
+		arvore.insereValor(arvore.raiz, gerador.nextInt(100)+1);
+		arvore.insereValor(arvore.raiz, gerador.nextInt(100)+1);
 
-		arvore.insereValor(arvore.raiz, 5);
-		arvore.insereValor(arvore.raiz, 10);
-		arvore.insereValor(arvore.raiz, 15);
-		arvore.insereValor(arvore.raiz, 20);
-		arvore.insereValor(arvore.raiz, 25);
 
-		arvore.imprimeArvore(arvore.raiz);
-		arvore.buscarValor(arvore.raiz, 10);	
-		arvore.liberaMemoria(arvore.raiz);
+        
+		arvore.imprimeArvore(arvore.getRaiz());
+		
+        System.out.println("Insira o grau de parentesco que voce deseja consultar: ");
+        grau = leitor.nextInt();
+
+        arvore.buscarValor(arvore.getRaiz(), grau);
+
+        System.out.println("Deseja deletar o parentesco: 1- DELETAR");
+        escolha = leitor.nextInt();
+
+        if (escolha == 1){
+            arvore.removeNo(arvore.getRaiz(), grau);
+            System.out.println("Grau de parentesco deletado !");
+        }
+        arvore.imprimeArvore(arvore.getRaiz());
+        arvore.liberaMemoria(arvore.getRaiz());
 	
 
+		
+		
 	}
 
 }
